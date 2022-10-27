83af265271901563da1fc031be788c73be62baad
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-10-27 20:49:26 +0300

    Create custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def update_post_counter
+    author.update(PostsCounter: author.posts.count)
+  end
+
+  def latest_five_comments
+    comments.includes(:post).order(created_at: :desc).limit(5)
+  end
 end
