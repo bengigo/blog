3a8d11b2542ed26110fb2fc1721789ad944a1ba7
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-10-28 14:29:40 +0300

    Test like models methods and validations with specs

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  RSpec.describe Like, type: :model do
+    subject do
+      user = User.new(Name: 'Bengi', Photo: 'ghttps://unsplash.com/photos/6dmx8YnkPGo', Bio: 'This is my bio', PostsCounter: '6')
+      p = Post.create(Title: 'Test Title', Text: 'Test Text Text', CommentsCounter: '0', LikesCounter: '0', author: user)
+      Like.create(author: user, post: p)
+      Like.create(author: user, post: p)
+    end
+  
+    it 'should update likes counter' do
+      expect(subject.update_likes_counter).to eq true
+      expect(subject.post.LikesCounter).to eq 2
+    end
+  end
