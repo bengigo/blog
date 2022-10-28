d496ffcda60e80cc6cea98d1d903eccf38d17ffb
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-10-28 14:21:52 +0300

    Test comments models methods and validations with specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    user = User.new(Name: 'Bengi', Photo: 'ghttps://unsplash.com/photos/6dmx8YnkPGo', Bio: 'This is my bio', PostsCounter: '6')
+    p = Post.create(Title: 'Test Title', Text: 'Test Text Text', CommentsCounter: '0', LikesCounter: '0', author: user)
+    Comment.create(Text: 'First comment', author: user, post: p)
+    Comment.create(Text: 'Second comment', author: user, post: p)
+  end
+
+  it 'should update comments counter' do
+    expect(subject.update_comments_counter).to eq true
+    expect(subject.post.CommentsCounter).to eq 2
+  end
 end
