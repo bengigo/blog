e38855e0efd47419e58c501f788b58b1a1017762
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-01 21:53:59 +0300

    Add route, view and controller for specific post

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   root "users#index"
   get "/users/:id", to: "users#show"
   get "users/:id/posts", to: "posts#index"
+  get "users/:id/posts/:id", to: "posts#show"
 end
