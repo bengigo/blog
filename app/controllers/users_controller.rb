516477c6cec4ff3aa1b94d3fcfdd47292fe271f4
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-01 21:22:09 +0300

    Add show method, view and route for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
 class UsersController < ApplicationController
-  @users = User.all
+  def index; end
+
+  def show; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
+  get "/users/:id", to: "users#show"
 end
