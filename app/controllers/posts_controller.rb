6f6a7d7d812de708123e50717eb053fa885496cf
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-03 19:43:06 +0300

    Add single post page view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,9 @@ class PostsController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @user_posts = @user.posts
-    @post = @user_posts.find(params[:id])
+    # @user = User.find(params[:id])
+    # @user_posts = @user.posts
+    # @post = @user_posts.find(params[:id])
+    @post = Post.find(params[:id])
   end
 end
