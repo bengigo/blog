17c1ec3273865f266736facedc8083fe63fba956
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-04 11:11:45 +0300

    Create a post for current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,20 @@ class PostsController < ApplicationController
   end
 
   def show
-    # @user = User.find(params[:id])
-    # @user_posts = @user.posts
-    # @post = @user_posts.find(params[:id])
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(Title: params[:post][:title], Text: params[:post][:text], author_id: current_user.id)
+    if @post.save
+      redirect_to "/users/#{current_user.id}/posts"
+
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
   get "users/:id/posts", to: "posts#index"
   get "users/:id/posts/:id", to: "posts#show"
+  get "/posts/new", to: "posts#new"
+  post "posts/new", to: "posts#create"
 end
