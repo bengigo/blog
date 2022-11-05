0ae09eb9cb44513a84250524e60e859b148fec41
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-05 14:10:20 +0300

    Change indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  has_many :comments, foreign_key: :author_id
-  has_many :posts, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
+    has_many :comments, foreign_key: :author_id
+    has_many :posts, foreign_key: :author_id
+    has_many :likes, foreign_key: :author_id
 
-  validates :Name, presence: true
-  validates :PostsCounter, numericality: { allow_blank: true, greater_than_or_equal_to: 0 }
+   validates :Name, presence: true
+   validates :PostsCounter, numericality: { allow_blank: true, greater_than_or_equal_to: 0 }
 
-  def latest_three_posts
-    posts.includes(:author).order(created_at: :desc).limit(3)
-  end
+    def latest_three_posts
+        posts.includes(:author).order(created_at: :desc).limit(3)
+   end
 end
