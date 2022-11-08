64e4a5f514ca76930d8c12b0dc4533a797f2c51e
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-08 22:53:01 +0300

    Fix likes counter

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,23 @@
+# class LikesController < ApplicationController
+#   def create
+#     @like = Like.new(post_id: params[:post_id], author_id: current_user.id)
+#     @like.save
+#     @like.update_likes_counter
+#   end
+# end
+
+
 class LikesController < ApplicationController
   def create
-    @like = Like.new(post_id: params[:post_id], author_id: current_user.id)
-    @like.save
+    post = Post.find(params[:post_id])
+    like = Like.new
+    like.author = current_user
+    like.post = post
+    if like.save
+      flash[:success] = 'Liked successfully'
+    else
+      flash[:error] = 'Error while trying to like'
+    end
+    redirect_to likes_new_path(current_user, post)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,21 @@
+# class Like < ApplicationRecord
+#   belongs_to :author, class_name: 'User'
+#   belongs_to :post
+
+#   after_save :update_likes_counter
+
+#   def update_likes_counter
+#     post.update(likes_counter: post.likes.count)
+#   end
+# end
+
 class Like < ApplicationRecord
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
   belongs_to :author, class_name: 'User'
-  belongs_to :post
 
-  after_save :update_likes_counter
+  after_save :update_like_counter
 
-  def update_likes_counter
-    post.update(likes_counter: post.likes.count)
+  def update_like_counter
+    post.increment!(:likes_counter)
   end
 end
