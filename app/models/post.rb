b0496f2f91e1e4d58b5a3a11cdcfeda5241bf5e7
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-08 20:12:27 +0300

    Change counter validations to accept only integer

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
 
   validates :Title, presence: true
   validates :Title, length: { maximum: 250 }
-  validates :CommentsCounter, numericality: { allow_blank: true, greater_than_or_equal_to: 0 }
-  validates :LikesCounter, numericality: { allow_blank: true, greater_than_or_equal_to: 0 }
+  validates :CommentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :LikesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_post_counter
     author.update(PostsCounter: author.posts.count)
