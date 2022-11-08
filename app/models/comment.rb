4ef699c734dbb43aba6b198ace2626651f24c5b9
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-08 20:02:54 +0300

    Remove unused comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  # validates :CommentsCounter, numericality: { allow_blank: true, greater_than_or_equal_to: 0 }
-
   def update_comments_counter
     post.update(CommentsCounter: post.comments.count)
   end
