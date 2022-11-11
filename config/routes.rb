28c7e123aefa68ffed5b1e872a4b7426b0ce9444
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-11 18:22:01 +0300

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@
 
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :api do
+    resources :people
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
